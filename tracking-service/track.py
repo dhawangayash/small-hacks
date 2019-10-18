05a7b345d4965d02193db1673252b1fdd424f257
Dhawan Gayash <dhawan_gajendrangayash@intuit.com>
2019-10-18 11:29:07 -0700

    Adding GET and OPTION endpoints support and ssl support

--- tracking-service/track.py
+++ tracking-service/track.py
@@ -3,7 +3,7 @@ from flask import Response
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/api/v2/cache/', methods=['GET', 'POST'])
+@app.route('/api/v2/cache/', methods=['GET', 'POST','OPTIONS'])
 def upload_file():
     if request.method == 'POST':
         payload = request.data
@@ -11,6 +11,13 @@ def upload_file():
             f.write(str(payload) + "\n")
         resp = Response('', status=200, mimetype='application/json')
         resp.headers['Link'] = 'http://localhost'
+        return resp
+    if request.method == 'GET' or request.method == 'OPTIONS':
+        with open('./ff.json', 'r') as f:
+            featureFlag = f.read()
+        resp = Response(featureFlag, status=200, mimetype='application/json')
+        resp.headers['Link'] = 'http://localhost'
+        resp.headers.add('Access-Control-Allow-Origin', '*')
         return resp
     else:
         return "hmm let's see"
